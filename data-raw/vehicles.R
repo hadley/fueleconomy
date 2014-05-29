3baaad723780b84873b0141e625d424d10c4b468
hadley <h.wickham@gmail.com>
2014-05-29 14:53:02 -0500

    Document data

--- data-raw/vehicles.R
+++ data-raw/vehicles.R
@@ -10,10 +10,12 @@ if (!file.exists("data-raw/vehicles.csv")) {
 raw <- read.csv("data-raw/vehicles.csv", stringsAsFactors = FALSE)
 
 vehicles <- raw %>%
+  tbl_df() %>%
   select(id, make, model, year, class = VClass, trans = trany, drive = drive,
     cyl = cylinders, displ = displ, fuel = fuelType, hwy = highway08,
     cty = city08) %>%
-  filter(drive != "")
+  filter(drive != "") %>%
+  arrange(make, model, year)
 
 save(vehicles, file = "data/vehicles.rdata")
 
@@ -21,4 +23,4 @@ common <- vehicles %>%
   group_by(make, model) %>%
   summarise(n = n(), years = n_distinct(year)) %>%
   filter(years >= 10)
-save(vehicles, file = "data/common.rdata")
+save(common, file = "data/common.rdata")
